f5b38199f6e69fd37b20ea5fac2dc88dfcfcfe27
JHOSSEP <jhossep7928@gmail.com>
2022-06-06 16:28:05 -0500

    first commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,24 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array[0];
+
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length-1];
+
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
+  
 }
 
 
@@ -23,6 +29,14 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  for (var i = 0; i < array.length; i++){
+    console.log('1', array);
+    array[i] = array[i] + 1;
+    console.log('2', array);
+  }
+  console.log('3', array);
+  return array;
+
 }
 
 
@@ -30,6 +44,9 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array;
+
 }
 
 
@@ -38,6 +55,10 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+  return array;
+
+
 }
 
 
@@ -47,21 +68,40 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-}
+  var frase = '';
+  for (var i = 0; i < palabras.length; i++){
+    console.log('1', frase);
+    if (palabras[i] === palabras[palabras.length - 1]){
+      frase = frase + palabras[i]; 
+    }else{
+      frase = frase + palabras[i] + ' ';
+      console.log('2', frase);
+    }
 
+    }
+    console.log('3', frase);
+  return frase;
+  }
+ 
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] === elemento) return true;
+  }
+  return false;
+  
+
 }
 
 
-function agregarNumeros(numeros) {
+function agregarNumeros(numeros) 
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
-}
+
 
 
 function promedioResultadosTest(resultadosTest) {
